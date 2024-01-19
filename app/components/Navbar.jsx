9f5753f6be6aa46af5f92f51e99f183493a18cb5
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-01-19 02:36:28 +0100

    Add Navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,38 @@
+'use client';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import Link from 'next/link';
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from '@ant-design/icons';
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+      <div className='logo-container'>
+        <Avatar src={'./vercel.svg'} size='large' />
+        <Typography.Title level={2} className='logo'>
+          <Link href='/'>Cryptochapter</Link>
+        </Typography.Title>
+      </div>
+      <Menu theme='dark'>
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link href={'/'}>Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link href={'/cryptocurrencies'}>Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link href={'/exchanges'}>Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link href={'/news'}>Home</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
